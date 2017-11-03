ae22f840eae8aca2af47ffaf0ba68a4648645c8d
Benoit Fievet <fievet.benoit@gmail.com>
2017-11-03 11:56:39 +0100

    add crisp file

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-crisp'
+  name: 'ember-cli-crisp',
+  contentFor: function (type, config) {
+    if (type === 'head-footer') {
+      if (!config.crisp || !config.crisp.WEBSITE_ID) {
+        return '';
+      }
+      return `<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="${config.crisp.WEBSITE_ID}";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>`;
+    }
+  }
 };
